4695bb7e2c4000da341dd508bfeb16f1237ed77c
esickert <esickert2@gmail.com>
2021-04-23 12:03:33 -0700

    plus

---
+++ src/main/java/Monster.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Monster {
+
+    @Test
+    public void monsterPOM()    {
+        System.out.println("This class is for developpment of a Page Object Model");
+    }
+
+    @Test
+    public void openWebPage()  {
+        System.setProperty("webdriver.chrome.driver","SeleniumDrivers//chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.monster.com");
+        driver.manage().window().maximize();
+
+        WebElement login = driver.findElement(By.cssSelector("#gatsby-focus-wrapper > div > header > div > div:nth-child(4) > div > a > svg > path"));
+        login.click();
+
+    }
+
+
+}
