06c2e7575704c06108dafde89b26e147739b3551
esickert <esickert2@gmail.com>
2021-07-16 21:03:50 -0700

    plus

--- src/main/java/TechFetch.java
+++ src/main/java/TechFetch.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TechFetch {
+public class TechFetch  {
 
     @Test
     public void techFetch() {
